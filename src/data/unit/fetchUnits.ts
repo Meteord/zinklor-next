f3918a37c1559b9cc30932c5da78da1d884b9e62
Johanna Schmitt <schmitt9@hm.edu>
2024-05-10 16:18:24 +0200

    Mehr Einheiten

--- src/data/unit/fetchUnits.ts
+++ src/data/unit/fetchUnits.ts
@@ -6,6 +6,10 @@ let König = require("./König.json");
 let Drachenkönig = require("./Drachenkönig.json");
 let Meeresjünger = require("./Meeresjünger.json");
 let Meereskönig = require("./Meereskönig.json");
+let Zauberer = require("./Zauberer.json");
+let Orks = require("./Orks.json");
+let Palastwache = require("./Palastwache des Zwergenkönigs.json");
+let Kobolde = require("./Kobolde.json");
 export const fetchUnits = () => {
   return [
     plainToInstance(Einheit, Engel),
@@ -13,5 +17,9 @@ export const fetchUnits = () => {
     plainToInstance(Einheit, Drachenkönig),
     plainToInstance(Einheit, Meeresjünger),
     plainToInstance(Einheit, Meereskönig),
+    plainToInstance(Einheit, Zauberer),
+    plainToInstance(Einheit, Orks),
+    plainToInstance(Einheit, Palastwache),
+    plainToInstance(Einheit, Kobolde),
   ];
 };
